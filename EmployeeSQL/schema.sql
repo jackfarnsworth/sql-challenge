fe747d624aca9be1039196e4021fb8c91cdcc273
jackcfarnsworth <jackcfarnsworth@gmail.com>
2022-02-22 20:54:24 -0500

    final touches

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,43 +1,36 @@
-DROP TABLE dept_emp_junction;
-DROP TABLE dept_manager_junction;
-DROP TABLE salaries;
-DROP TABLE departments;
-DROP TABLE employees;
-DROP TABLE titles;
-
 CREATE TABLE titles (
-    title_id VARCHAR PRIMARY KEY NOT NULL,
-    title VARCHAR NOT NULL
+    title_id VARCHAR(5) PRIMARY KEY NOT NULL,
+    title VARCHAR(18) NOT NULL
 );
 
 CREATE TABLE employees (
     emp_no INT PRIMARY KEY NOT NULL,
-    emp_title VARCHAR NOT NULL,
+    emp_title VARCHAR(5) NOT NULL,
 	FOREIGN KEY (emp_title)
 	REFERENCES titles(title_id),
     birth_date DATE NOT NULL,
-    first_name VARCHAR NOT NULL,
-    last_name VARCHAR NOT NULL,
-    sex VARCHAR NOT NULL,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    sex VARCHAR(1) NOT NULL,
     hire_date DATE NOT NULL
 );
 
 CREATE TABLE departments (
-    dept_no VARCHAR PRIMARY KEY NOT NULL,
-    dept_name VARCHAR
+    dept_no VARCHAR(4) PRIMARY KEY NOT NULL,
+    dept_name VARCHAR(18)
 );
 
 CREATE TABLE dept_emp_junction (
     emp_no INT NOT NULL,
 	FOREIGN KEY(emp_no)
 	REFERENCES employees(emp_no),
-    dept_no VARCHAR NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
 	FOREIGN KEY(dept_no)
 	REFERENCES departments(dept_no)
 );
 
 CREATE TABLE dept_manager_junction (
-    dept_no VARCHAR NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
 	FOREIGN KEY(dept_no)
 	REFERENCES departments(dept_no),
     emp_no INT NOT NULL,
